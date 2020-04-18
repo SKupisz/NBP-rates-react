92233e72085cc2fbcc32ee29d407dd45fd54b5af
SKupisz <szym-ku@wp.pl>
2020-04-18 15:46:35 +0200

    Added preloader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Preloader from "./preloader.jsx";
+import github from "../img/github.png";
 
 export default class Main extends React.Component{
     constructor(props){
@@ -22,15 +24,18 @@ export default class Main extends React.Component{
             <div className="main-container">
                 <header className="main-header">C Table Rates</header>
                 {
-                    this.state.currData == [] ? <h1 className = "loading-bar">loading</h1>: this.state.currData.map(data => 
+                    this.state.currData == [] ? <Preloader/>: this.state.currData.map(data => 
 
-                    <div className = "row">
-                        <div className="row-item">{data.code}</div>
-                        <div className="row-item">{data.bid}</div>
-                        <div className="row-item">{data.ask}</div>
-                        
-                    </div>)
+<div className = "row">
+    <div className="row-item">{data.code}</div>
+    <div className="row-item">{data.bid}</div>
+    <div className="row-item">{data.ask}</div>
+    
+</div>)
                 }
+                <a href = "https://github.com/SKupisz/NBP-rates-react">
+                    <div className="github-button"><img src={github} alt="" className = "button-img"/></div>
+                </a>
             </div>
         );
     }
---
+++ src/components/preloader.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+
+export default class Preloader extends React.Component{
+    render(){
+        return(
+<div className="preloader">
+                        <header className="loader-header">Loading...</header>
+                        <section className="grid-container">
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+                                <div className="grid-item item1"></div>
+
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2 "></div>
+                                <div className="grid-item item2"></div>
+                                <div className="grid-item item2"></div>
+
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3 "></div>
+                                <div className="grid-item item3"></div>
+                                <div className="grid-item item3"></div>
+
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4 "></div>
+                                <div className="grid-item item4"></div>
+                                <div className="grid-item item4"></div>
+
+                                <div className="grid-item item5"></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5"></div>
+                                <div className="grid-item item5"></div>
+                                <div className="grid-item item5"></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5"></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5 "></div>
+                                <div className="grid-item item5"></div>
+
+                                <div className="grid-item item6"></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6"></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6"></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6"></div>
+                                <div className="grid-item item6 "></div>
+                                <div className="grid-item item6"></div>
+
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7 "></div>
+                                <div className="grid-item item7 "></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7 "></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+                                <div className="grid-item item7"></div>
+
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8 "></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8 "></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+                                <div className="grid-item item8"></div>
+
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9 "></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9 "></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+                                <div className="grid-item item9"></div>
+
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10 "></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10 "></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+                                <div className="grid-item item10"></div>
+
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11 "></div>
+                                <div className="grid-item item11 "></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11 "></div>
+                                <div className="grid-item item11 "></div>
+                                <div className="grid-item item11"></div>
+                                <div className="grid-item item11"></div>
+
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12 "></div>
+                                <div className="grid-item item12 "></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12 "></div>
+                                <div className="grid-item item12 "></div>
+                                <div className="grid-item item12"></div>
+                                <div className="grid-item item12"></div>
+
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13 "></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13 "></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+                                <div className="grid-item item13"></div>
+
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14 "></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14 "></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+                                <div className="grid-item item14"></div>
+
+
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                                <div className="grid-item item15"></div>
+                        </section>
+                    </div>
+        );
+    }
+}
